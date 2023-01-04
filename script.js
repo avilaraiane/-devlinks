76d9ac63135a2b6bbbd70be8b6b8b16ac86481ee
avila <avilateen12@gmail.com>
2023-01-04 12:01:45 -0300

    initial commit

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ function toggleMode() {
 
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/avatar-light.png")
+
   } else {
     img.setAttribute("src", "./assets/avatar.png")
+    
   }
 }
